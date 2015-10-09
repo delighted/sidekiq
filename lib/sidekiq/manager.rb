efd86ccd3bafba1c7d65a0ba9dd5825c33161075
Mike Perham <mperham@gmail.com>
2015-10-09 12:31:30 -0700

    Fix thread leak, fixes #2598

--- lib/sidekiq/manager.rb
+++ lib/sidekiq/manager.rb
@@ -119,7 +119,7 @@ module Sidekiq
     # get handle to the underlying thread performing work for a processor
     # so we have it call us and tell us.
     def real_thread(proxy_id, thr)
-      @threads[proxy_id] = thr
+      @threads[proxy_id] = thr if thr.alive?
     end
 
     def procline(tag)
