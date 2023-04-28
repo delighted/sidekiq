ba18dca829b0fb946de73ce7634d73e74e8dd911
Erik Pilz <erik@delighted.com>
2023-04-27 20:35:01 -0700

    Redis-rb 4.8.0 compatibility fixes
    See https://github.com/sidekiq/sidekiq/commit/09dacfed8f3f0b42833122b138639d22c71e3d56

--- lib/sidekiq/fetch.rb
+++ lib/sidekiq/fetch.rb
@@ -130,8 +130,14 @@ module Sidekiq
     # recreate the queue command each time we invoke Redis#blpop
     # to honor weights and avoid queue starvation.
     def queues_cmd
-      queues = @strictly_ordered_queues ? @unique_queues.dup : @queues.shuffle.uniq
-      queues << Sidekiq::Fetcher::TIMEOUT
+      if @strictly_ordered_queues
+        @unique_queues.dup
+      else
+        permute = @queues.shuffle
+        permute.uniq!
+        permute << {timeout: TIMEOUT}
+        permute
+      end
     end
   end
 end
